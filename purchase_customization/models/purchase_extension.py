d573e81ef3e527172db92849526468ce95e29865
Asiimire Patricia <101188715+asiimire@users.noreply.github.com>
2024-09-26 15:28:10 +0300

    Delete purchase_customization directory

--- purchase_customization/models/purchase_extension.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    vendor_ids = fields.Many2many(
-        'res.partner', string="Vendors", domain=[('supplier_rank', '>', 0)]
-    )
-    winning_bid_id = fields.Many2one('purchase.bid', string="Winning Bid")
